f68d3d5ea54bd2c2862874ac3bbb0da38c343451
Daniela <113726863+danielacaicedo03@users.noreply.github.com>
2024-04-14 12:34:44 -0500

    Navegación Registro

--- Aplicacion/App.tsx
+++ Aplicacion/App.tsx
@@ -2,6 +2,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from './App/paginas/Login';
+import Registro from './App/paginas/Registro';
 
 const Stack = createNativeStackNavigator();
 
@@ -9,10 +10,12 @@ export default function App() {
   
   return (<NavigationContainer>
     <Stack.Navigator initialRouteName="Login">
-      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Login" component={Login} options={{headerShown:false}} />
+      <Stack.Screen name="Registro" component={Registro} options={{headerShown:false}} />
     </Stack.Navigator>
   </NavigationContainer>);
    
+
 }
 
 const styles = StyleSheet.create({
---
+++ Aplicacion/App/paginas/Registro.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Registro = () => {
+  return (
+    <View>
+      <Text>Registro</Text>
+    </View>
+  )
+}
+
+export default Registro
