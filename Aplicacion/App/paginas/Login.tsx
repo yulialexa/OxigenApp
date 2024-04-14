b1d935b215c40f7830f13e4f4a4f7e12c7047b3b
Daniela <113726863+danielacaicedo03@users.noreply.github.com>
2024-04-14 13:07:39 -0500

    Logo

--- Aplicacion/App/paginas/Login.tsx
+++ Aplicacion/App/paginas/Login.tsx
@@ -1,6 +1,7 @@
 import { NavigationProp } from '@react-navigation/native';
-import { View, Text } from 'react-native'
+import { View, Text, Image, StyleSheet } from 'react-native'
 import React from 'react'
+
 
 interface RouterProps {
     navigation: NavigationProp<any, any>;
@@ -8,10 +9,31 @@ interface RouterProps {
 
 const Login = ({navigation}: RouterProps ) => {
       return (
-    <View>
+    <View style= {styles.container}>
+        <Image style={styles.logo} source={require("../../assets/Logo.png")}/>
       <Text>Login</Text>
-    </View>
+          </View>
   )
 }
 
 export default Login
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#EEEEEE',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    logo: {
+        width: 250,
+        height: 250,
+        zIndex: 1,
+        alignItems: 'center',
+        alignSelf: 'center',
+        position: 'absolute',
+        top: 50,
+        // resizeMode: 'contain',
+      },
+
+  });
