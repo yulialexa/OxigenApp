38a3baa1f0f5a3ce928309a9cfbe482c17df2675
Yuli <166955635+yulialexa@users.noreply.github.com>
2024-04-14 22:31:16 -0500

    Revert "Implementación de inputs de login"
    This reverts commit dd2b688bedd5615cd8b370471d8ee662c05d7ca9.

--- Aplicacion/App/paginas/Login.tsx
+++ Aplicacion/App/paginas/Login.tsx
@@ -1,27 +1,17 @@
 import { NavigationProp } from '@react-navigation/native';
-import { View, Text, Image, StyleSheet, TextInput, KeyboardAvoidingView } from 'react-native'
-import React,{useState} from 'react'
-import { FIREBASE_AUTH } from '../../Firebase/config';
+import { View, Text, Image, StyleSheet } from 'react-native'
+import React from 'react'
+
 
 interface RouterProps {
     navigation: NavigationProp<any, any>;
   }
 
 const Login = ({navigation}: RouterProps ) => {
-
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [loading, setLoading] = useState(false);
-  const auth = FIREBASE_AUTH;
-
       return (
     <View style= {styles.container}>
         <Image style={styles.logo} source={require("../../assets/Logo.png")}/>
-      <Text>OXIGENAPP</Text> 
-      <KeyboardAvoidingView behavior='padding'>
-        <TextInput style={styles.input} value={email} placeholder='Ingrese su email:' autoCapitalize='none' onChangeText={(text)=> setEmail(text)}></TextInput>
-        <TextInput secureTextEntry={true} style={styles.input} value={password} placeholder='Ingrese su contraseña:' autoCapitalize='none' onChangeText={(text)=> setPassword(text)}></TextInput>
-        </KeyboardAvoidingView>
+      <Text>Login</Text>
           </View>
   )
 }
@@ -45,22 +35,5 @@ const styles = StyleSheet.create({
         top: 50,
         // resizeMode: 'contain',
       },
-      input: {
-        marginVertical: 10,
-        marginHorizontal: 20,
-        height: 50,
-        borderWidth: 1,
-        borderColor: '#ccc',
-        borderRadius: 25,
-        padding: 10,
-        backgroundColor: '#5d524c',
-        color: '#ffffff',
-        fontSize: 14,
-        fontWeight: 'bold',
-        textAlign: 'center',
-        width: '80%', 
-        
-      },
-      
 
   });
