afeda5c2d247ec943c78984229430b3824b6790a
Yuli <166955635+yulialexa@users.noreply.github.com>
2024-04-14 22:33:47 -0500

    Implementación de inputs login

--- Aplicacion/App/paginas/Login.tsx
+++ Aplicacion/App/paginas/Login.tsx
@@ -1,17 +1,27 @@
 import { NavigationProp } from '@react-navigation/native';
-import { View, Text, Image, StyleSheet } from 'react-native'
-import React from 'react'
-
+import { View, Text, Image, StyleSheet, TextInput, KeyboardAvoidingView } from 'react-native'
+import React,{useState} from 'react'
+import { FIREBASE_AUTH } from '../../Firebase/config';
 
 interface RouterProps {
     navigation: NavigationProp<any, any>;
   }
 
 const Login = ({navigation}: RouterProps ) => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  const auth = FIREBASE_AUTH;
+
       return (
     <View style= {styles.container}>
         <Image style={styles.logo} source={require("../../assets/Logo.png")}/>
-      <Text>Login</Text>
+      <Text>OXIGENAPP</Text> 
+      <KeyboardAvoidingView behavior='padding'>
+        <TextInput style={styles.input} value={email} placeholder='Ingrese su email:' autoCapitalize='none' onChangeText={(text)=> setEmail(text)}></TextInput>
+        <TextInput secureTextEntry={true} style={styles.input} value={password} placeholder='Ingrese su contraseña:' autoCapitalize='none' onChangeText={(text)=> setPassword(text)}></TextInput>
+        </KeyboardAvoidingView>
           </View>
   )
 }
@@ -35,5 +45,22 @@ const styles = StyleSheet.create({
         top: 50,
         // resizeMode: 'contain',
       },
+      input: {
+        marginVertical: 10,
+        marginHorizontal: 20,
+        height: 50,
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 25,
+        padding: 10,
+        backgroundColor: '#5d524c',
+        color: '#ffffff',
+        fontSize: 14,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        width: '80%', 
+        
+      },
+      
 
   });
