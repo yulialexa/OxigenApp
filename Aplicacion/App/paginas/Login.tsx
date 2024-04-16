7516a166d5564af417ff4e482e0a8a178b70178f
Yuli <166955635+yulialexa@users.noreply.github.com>
2024-04-15 19:15:01 -0500

    Revert "Merge branch 'main' of https://github.com/yulialexa/OxigenApp"
    This reverts commit afbbc54d9f40794de5f1ea810b41ca38f9d0c363, reversing
    changes made to 3afac4f4f5aa44a1074ddb59e87d83aaf79eba6d.

--- Aplicacion/App/paginas/Login.tsx
+++ Aplicacion/App/paginas/Login.tsx
@@ -1,6 +1,5 @@
 import { NavigationProp } from '@react-navigation/native';
-import { signInWithEmailAndPassword } from 'firebase/auth';
-import { ActivityIndicator ,View, Text, Image, StyleSheet, TextInput, KeyboardAvoidingView, TouchableOpacity } from 'react-native'
+import { View, Text, Image, StyleSheet, TextInput, KeyboardAvoidingView } from 'react-native'
 import React,{useState} from 'react'
 import { FIREBASE_AUTH } from '../../Firebase/config';
 
@@ -15,61 +14,15 @@ const Login = ({navigation}: RouterProps ) => {
   const [loading, setLoading] = useState(false);
   const auth = FIREBASE_AUTH;
 
-  const singIn = async () => {
-    setLoading(true);
-    try{
-      const response = await signInWithEmailAndPassword(auth, email, password);
-      console.log(response);
-
-    } catch (error: any) {
-      console.log(error);
-      alert('Iniciar Sesión fallido: '+ error.message);
-    } finally {
-      setLoading(false);
-    }
-  }
-
-  const goToRecuperarConstraseña = () => {
-    navigation.navigate('RecuperarContraseña');
-  }
-
-  const goToRegister = () => {
-    navigation.navigate('Registro'); // Navega a la pantalla de registro
-  };
-
-
-
       return (
     <View style= {styles.container}>
         <Image style={styles.logo} source={require("../../assets/Logo.png")}/>
       <Text>OXIGENAPP</Text> 
-      <Text>¡Bienvenido!</Text>
       <KeyboardAvoidingView behavior='padding'>
         <TextInput style={styles.input} value={email} placeholder='Ingrese su email:' autoCapitalize='none' onChangeText={(text)=> setEmail(text)}></TextInput>
         <TextInput secureTextEntry={true} style={styles.input} value={password} placeholder='Ingrese su contraseña:' autoCapitalize='none' onChangeText={(text)=> setPassword(text)}></TextInput>
-        
-        { loading ? (
-            <ActivityIndicator size="large" color="#0000" />
-        ) : (
-            <>
-              <TouchableOpacity style={styles.button2} onPress={goToRecuperarConstraseña}>
-                <Text style={styles.buttonText2}>¿Olvidó su Contraseña?</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={styles.button1} onPress={singIn}>
-                <Text style={styles.buttonText}>Entrar</Text>
-              </TouchableOpacity>
-
-              <TouchableOpacity style={styles.button2} onPress={goToRegister}>
-                <Text style={styles.buttonText2}>¡Inscribete!</Text>
-              </TouchableOpacity>
-            </>
-        )}
- 
-        
         </KeyboardAvoidingView>
           </View>
-
-          
   )
 }
 
@@ -83,8 +36,8 @@ const styles = StyleSheet.create({
       justifyContent: 'center',
     },
     logo: {
-        width: 170,
-        height: 170,
+        width: 250,
+        height: 250,
         zIndex: 1,
         alignItems: 'center',
         alignSelf: 'center',
@@ -106,33 +59,7 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         textAlign: 'center',
         width: '80%', 
-      },
-      button1: {
-        backgroundColor: '#cff9fd',
-        padding: 10,
-        borderRadius: 5,
-        marginHorizontal: 65,
-        alignItems: 'center',
-        // alignSelf: 'center',
-        marginTop: 10
-      },
-      button2: {
-        // backgroundColor: '#cff9fd',
-        padding: 10,
-        borderRadius: 5,
-        // alignItems: 'center',
-        // width: '80%',
-        // alignSelf: 'center',
-        marginTop: 10
-      },
-      buttonText: {
-        color: '#000000',
-        fontSize: 16
-      },
-      buttonText2: {
-        textAlign:'center',
-        color: '#000000',
-        fontSize: 12
+        
       },
       
 
